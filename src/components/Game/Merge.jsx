af92c6c2f36a0351cdda05463dcf45d06132af8d
minju0809 <loverminju88@naver.com>
2023-06-07 23:27:16 +0900

    타이머까지 추가

--- src/components/Game/Merge.jsx
+++ src/components/Game/Merge.jsx
@@ -1,30 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
-{
-  /* <header>
-  <Link to="/game">뒤로</Link>
-  <h1>Merge Game</h1>
-</header>; */
-}
 
 const Merge = () => {
   const width = 7;
   const height = 9;
+  const initialGrid = Array(height).fill().map(() => Array(width).fill(null));
 
-  const [grid, setGrid] = useState(() => {
-    const initialGrid = Array(height)
-      .fill()
-      .map(() => Array(width).fill(null));
-    return initialGrid;
-  });
-
+  const [grid, setGrid] = useState(initialGrid);
   const [popupVisible, setPopupVisible] = useState(false);
-
   const [selectedCell, setSelectedCell] = useState(null);
+  const [timer, setTimer] = useState(0);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [stoppedTime, setStoppedTime] = useState(0);
 
   const handleAddNumber = () => {
     const emptyCells = [];
+
     grid.forEach((row, rowIndex) => {
       row.forEach((cell, colIndex) => {
         if (cell === null) {
@@ -36,11 +28,14 @@ const Merge = () => {
     if (emptyCells.length === 0) {
       setPopupVisible(true);
     } else {
-      const randomCell =
-        emptyCells[Math.floor(Math.random() * emptyCells.length)];
+      const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
       const newGrid = [...grid];
       newGrid[randomCell.row][randomCell.col] = 1;
       setGrid(newGrid);
+
+      if (!timerStarted) {
+        setTimerStarted(true);
+      }
     }
   };
 
@@ -58,7 +53,7 @@ const Merge = () => {
           const firstValue = newGrid[selectedCell.row][selectedCell.col];
           const secondValue = newGrid[row][col];
 
-          if (firstValue === secondValue && firstValue !== 9) {
+          if (firstValue === secondValue && firstValue !== 5) {
             newGrid[selectedCell.row][selectedCell.col] = null;
             newGrid[row][col] = firstValue + 1;
           }
@@ -74,6 +69,36 @@ const Merge = () => {
     setPopupVisible(false);
   };
 
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60000).toString().padStart(2, '0');
+    const seconds = Math.floor((time % 60000) / 1000).toString().padStart(2, '0');
+    const milliseconds = (time % 1000).toString().padStart(3, '0');
+    return `${minutes}:${seconds}:${milliseconds}`;
+  };
+
+  useEffect(() => {
+    let intervalId = null;
+
+    if (timerStarted && !popupVisible && !grid.flat().includes(5)) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 100);
+      }, 100);
+    } else {
+      clearInterval(intervalId);
+
+      if (timerStarted && grid.flat().includes(5)) {
+        setTimerStarted(false);
+        setStoppedTime((prevTimer) => prevTimer + timer);
+      }
+
+      setTimer(0);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [timerStarted, popupVisible, grid, timer]);
+
   return (
     <div>
       <header>
@@ -82,6 +107,7 @@ const Merge = () => {
       </header>
       <div className="merge-container">
         <button onClick={handleAddNumber}>추가</button>
+        <div className="timer">{formatTime(timerStarted ? timer : stoppedTime)}</div>
         {grid.map((row, rowIndex) => (
           <div key={rowIndex} className="row">
             {row.map((cell, colIndex) => (
