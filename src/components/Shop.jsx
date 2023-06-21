c35adf37b54dce4f98cadc5a1c99d089ba610e82
minju0809 <loverminju88@naver.com>
2023-06-21 16:01:59 +0900

    shop 코드 정리

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -7,7 +7,6 @@ const Shop = () => {
   const [clickedText, setClickedText] = useState(""); // 클릭한 글자 상태
   const [showAllItem, setShowAllItem] = useState(false); // 첫 화면에서는 전체 상품 보이도록 설정
   const [filterByCategory, setFilterByCategory] = useState("전체상품"); // 카테고리 필터 상태
-  const [favorite, setFavorite] = useState("");
   const [favorites, setFavorites] = useState([]); // 즐겨찾기 목록을 저장할 배열
 
   const handleClick = (text) => {
@@ -15,15 +14,9 @@ const Shop = () => {
   };
 
   const handleFilter = (category) => {
-    if (category === "전체상품") {
-      handleClick(category); // 클릭한 글자 처리
-      setShowAllItem(!showAllItem);
-      setFilterByCategory("전체상품"); // 필터 초기화
-    } else {
-      handleClick(category); // 클릭한 글자 처리
-      setShowAllItem(false);
-      setFilterByCategory(category);
-    }
+    handleClick(category); // 클릭한 글자 처리
+    setShowAllItem(category === "전체상품" ? !showAllItem : false);
+    setFilterByCategory(category);
   };
 
   // 카테고리와 필터 조건을 정의한 객체
@@ -31,7 +24,7 @@ const Shop = () => {
     전체상품: () => true, // 모든 상품
     라켓: (product) => product.title.includes("라켓"), // '라켓'이 들어간 상품
     의류: (product) => product.title.includes("의류"), // '의류'가 들어간 상품
-    가방: (product) => product.title.includes("가방"), // '가방'아 들어간 상품
+    가방: (product) => product.title.includes("가방"), // '가방'이 들어간 상품
     신발: (product) => product.title.includes("신발"), // '신발'이 들어간 상품
     셔틀콕: (product) => product.title.includes("셔틀콕"), // '셔틀콕'이 들어간 상품
     용품: (product) => product.title.includes("용품"), // '용품'이 들어간 상품
@@ -39,39 +32,19 @@ const Shop = () => {
   };
 
   const getFilteredProducts = () => {
-    const filterCondition = categoryFilters[filterByCategory] || (() => false);
+    const filterCondition = categoryFilters[filterByCategory];
     return shopData.filter(filterCondition);
-  };
-
-  const getFavoriteProducts = () => {
-    const filterCondition = categoryFilters[filterByCategory] || (() => false);
-    const filteredProducts = shopData.filter(filterCondition);
-
-    if (filterByCategory === "즐겨찾기") {
-      return filteredProducts.filter((product) =>
-        favorites.includes(product.id)
-      );
-    }
-
-    return filteredProducts;
   };
 
   const handleFavorite = (productId) => {
     setFavorites((prevFavorites) => {
       const isFavorite = prevFavorites.includes(productId);
-      if (isFavorite) {
-        // 이미 즐겨찾기에 있는 상품인 경우 제거
-        const newFavorites = prevFavorites.filter((id) => id !== productId);
-        localStorage.setItem("favorites", JSON.stringify(newFavorites));
-        return newFavorites;
-      } else {
-        // 현재 즐겨찾기에 없는 상품인 경우 추가
-        const newFavorites = [...prevFavorites, productId];
-        localStorage.setItem("favorites", JSON.stringify(newFavorites));
-        return newFavorites;
-      }
+      const newFavorites = isFavorite
+        ? prevFavorites.filter((id) => id !== productId)
+        : [...prevFavorites, productId];
+      localStorage.setItem("favorites", JSON.stringify(newFavorites));
+      return newFavorites;
     });
-    setFavorite("");
   };
 
   useEffect(() => {
@@ -92,52 +65,28 @@ const Shop = () => {
       </header>
       <nav id="Shop_navbar">
         <ul className="nav-list">
-          <li className="nav-item" onClick={() => handleFilter("전체상품")}>
-            전체상품
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("라켓")}>
-            라켓
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("의류")}>
-            의류
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("가방")}>
-            가방
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("신발")}>
-            신발
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("셔틀콕")}>
-            셔틀콕
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("용품")}>
-            용품
-          </li>
-          <li className="nav-item" onClick={() => handleFilter("즐겨찾기")}>
-            즐겨찾기
-          </li>
+          {Object.keys(categoryFilters).map((category) => (
+            <li
+              key={category}
+              className="nav-item"
+              onClick={() => handleFilter(category)}
+            >
+              {category}
+            </li>
+          ))}
         </ul>
       </nav>
       <main className="main">
         <h2 className="section-title">{clickedText}</h2>
         <section className="product-list">
-          {filterByCategory === "favorites"
-            ? getFavoriteProducts().map((product) => (
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                  handleFavorite={handleFavorite}
-                  isFavorite={favorites.includes(product.id)}
-                />
-              ))
-            : getFilteredProducts().map((product) => (
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                  handleFavorite={handleFavorite}
-                  isFavorite={favorites.includes(product.id)}
-                />
-              ))}
+          {getFilteredProducts().map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              handleFavorite={handleFavorite}
+              isFavorite={favorites.includes(product.id)}
+            />
+          ))}
         </section>
       </main>
     </div>
